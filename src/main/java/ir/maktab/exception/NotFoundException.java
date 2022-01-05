fa7c4b42ffcaaad0d5b1a4124f31d2667a6fc68e
Mahsa <mahsa.alikhany@gmail.com>
2022-01-05 17:31:07 +0330

    add NotFoundException constructor

--- src/main/java/ir/maktab/exception/NotFoundException.java
+++ src/main/java/ir/maktab/exception/NotFoundException.java
@@ -1,4 +1,11 @@
-package ir.maktab.exception;/**
+package ir.maktab.exception;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class NotFoundException {
+ */
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
