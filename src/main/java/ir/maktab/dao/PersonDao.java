e7681eeabfd45d7e4c3f2458bc2270b3eab1b00f
Mahsa <mahsa.alikhany@gmail.com>
2022-01-05 13:57:37 +0330

    add AddPersonTest file

--- src/main/java/ir/maktab/dao/PersonDao.java
+++ src/main/java/ir/maktab/dao/PersonDao.java
@@ -1,7 +1,24 @@
 package ir.maktab.dao;
 
+import ir.maktab.model.entity.Person;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author Mahsa Alikhani m-58
  */
+@Repository
+@RequiredArgsConstructor
 public class PersonDao {
+    private SessionFactory sessionFactory;
+
+    public void save(Person person){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(person);
+        session.getTransaction().commit();
+        session.close();
+    }
 }
--- src/main/java/ir/maktab/service/PersonService.java
+++ src/main/java/ir/maktab/service/PersonService.java
@@ -1,4 +1,19 @@
-package ir.maktab.service;/**
+package ir.maktab.service;
+
+import ir.maktab.dao.PersonDao;
+import ir.maktab.model.entity.Person;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class PersonService {
+ */
+@Service
+@RequiredArgsConstructor
+public class PersonService {
+    private final PersonDao personDao;
+
+    public void add(Person person){
+        personDao.save(person);
+    }
 }
