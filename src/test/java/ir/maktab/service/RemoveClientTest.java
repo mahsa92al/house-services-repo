df83fdfebd41ef240e787dbcc5d0e43f9f9f8f7b
Mahsa <mahsa.alikhany@gmail.com>
2022-01-05 21:36:09 +0330

    rename give_Client_when_Add_Calls_Then_Exception_Return() to give_Client_when_Remove_Calls_Then_Exception_Return()

--- src/test/java/ir/maktab/service/RemoveClientTest.java
+++ src/test/java/ir/maktab/service/RemoveClientTest.java
@@ -1,10 +1,9 @@
 package ir.maktab.service;
 
 import ir.maktab.config.SpringConfig;
-import ir.maktab.exception.DuplicateClientException;
 import ir.maktab.exception.NotFoundException;
 import ir.maktab.model.entity.Client;
-import ir.maktab.model.enumaration.ClientStatus;
+import ir.maktab.model.enumaration.UserStatus;
 import ir.maktab.model.enumaration.Role;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,12 +26,12 @@ public class RemoveClientTest {
         client.setLastName("alikhani");
         client.setEmail("mahsa.alikhani@gmail.com");
         client.setPassword("123");
-        client.setClientStatus(ClientStatus.NEW);
+        client.setUserStatus(UserStatus.NEW);
         client.setRole(Role.CLIENT);
     }
 
     @Test
-    void give_Client_when_Add_Calls_Then_Exception_Return(){
+    void give_Client_when_Remove_Calls_Then_Exception_Return(){
         NotFoundException result = assertThrows(NotFoundException.class, ()->
                 clientService.remove(client));
         assertEquals("client not found!", result.getMessage());
