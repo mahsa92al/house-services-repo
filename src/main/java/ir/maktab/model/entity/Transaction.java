87966db2c4d0e1c6cf095fe4e448e5db47cf7e95
Mahsa <mahsa.alikhany@gmail.com>
2022-01-06 23:25:19 +0330

    add some properties to Transaction file

--- src/main/java/ir/maktab/model/entity/Transaction.java
+++ src/main/java/ir/maktab/model/entity/Transaction.java
@@ -1,4 +1,33 @@
-package ir.maktab.model.entity;/**
+package ir.maktab.model.entity;
+
+import ir.maktab.model.enumaration.TransactionType;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.security.PrivateKey;
+import java.util.Date;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class Transaction {
+ */
+@Setter
+@Getter
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date transactionDate;
+    @OneToOne
+    private Client client;
+    @OneToOne
+    private Expert expert;
+    @OneToOne
+    private Order order;
 }
