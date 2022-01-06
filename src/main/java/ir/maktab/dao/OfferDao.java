13a9e84f1165cc9383a6091ddc5ba1e98199fd9f
Mahsa <mahsa.alikhany@gmail.com>
2022-01-06 22:16:16 +0330

    add CRUD methods to Offer entity

--- src/main/java/ir/maktab/dao/OfferDao.java
+++ src/main/java/ir/maktab/dao/OfferDao.java
@@ -1,4 +1,67 @@
-package ir.maktab.dao;/**
+package ir.maktab.dao;
+
+import ir.maktab.model.entity.Offer;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class OfferDao {
+ */
+@Repository
+@RequiredArgsConstructor
+public class OfferDao {
+    private final SessionFactory sessionFactory;
+
+    public void save(Offer offer) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(offer);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(Offer offer) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query hql = session.createQuery("delete Offer o where o.id=:id");
+        hql.setParameter("id", offer.getId());
+        hql.executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public Optional<Offer> findOfferById(Offer offer) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query<Offer> hql = session.createQuery("from Offer o where o.id=:id");
+        hql.setParameter("id", offer.getId());
+        List<Offer> list = hql.getResultList();
+        session.getTransaction().commit();
+        session.close();
+        return Optional.ofNullable(list.isEmpty() ? null : list.get(0));
+    }
+
+    public void update(Offer offer) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.saveOrUpdate(offer);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public List<Offer> findAllOffers() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query<Offer> hql = session.createQuery("from Offer");
+        List<Offer> list = hql.getResultList();
+        session.getTransaction().commit();
+        session.close();
+        return list;
+    }
 }
--- src/main/java/ir/maktab/service/OfferService.java
+++ src/main/java/ir/maktab/service/OfferService.java
@@ -1,4 +1,49 @@
-package ir.maktab.service;/**
+package ir.maktab.service;
+
+import ir.maktab.dao.OfferDao;
+import ir.maktab.exception.NotFoundException;
+import ir.maktab.model.entity.Offer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class OfferService {
+ */
+@Service
+@RequiredArgsConstructor
+public class OfferService {
+    private final OfferDao offerDao;
+
+    public void addOffer(Offer offer){
+        offerDao.save(offer);
+    }
+
+    public void removeOffer(Offer offer){
+        Optional<Offer> foundOffer = findOfferById(offer);
+        if(foundOffer.isEmpty())
+            throw new NotFoundException("offer not found!");
+        offerDao.delete(offer);
+    }
+
+    private Optional<Offer> findOfferById(Offer offer) {
+        Optional<Offer> foundOffer = offerDao.findOfferById(offer);
+        return foundOffer;
+    }
+
+    public void update(Offer offer){
+        Optional<Offer> foundOffer = findOfferById(offer);
+        if(foundOffer.isEmpty())
+            throw new NotFoundException("offer not found!");
+        offerDao.update(offer);
+    }
+
+    public List<Offer> getAllOrders(){
+        List<Offer> offers = offerDao.findAllOffers();
+        if(offers.isEmpty())
+            throw new NotFoundException("there is no offer!");
+        return offers;
+    }
 }
