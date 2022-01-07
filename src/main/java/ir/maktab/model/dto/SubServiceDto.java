7030626da808550065abb59d67697c12b673c0ba
Mahsa <mahsa.alikhany@gmail.com>
2022-01-07 23:02:33 +0330

    add some properties to SubServiceDto

--- src/main/java/ir/maktab/model/dto/SubServiceDto.java
+++ src/main/java/ir/maktab/model/dto/SubServiceDto.java
@@ -1,4 +1,19 @@
-package ir.maktab.model.dto;/**
+package ir.maktab.model.dto;
+
+import ir.maktab.model.entity.Expert;
+import ir.maktab.model.entity.Order;
+import ir.maktab.model.entity.Service;
+
+import java.util.List;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class SubServiceDto {
+ */
+public class SubServiceDto {
+    private String title;
+    private Double basePrice;
+    private String description;
+    private List<Expert> experts;
+    private Service service;
+    private List<Order> orders;
 }
