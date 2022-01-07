153cafed621b83a30c1daebd038fb27df18925f7
Mahsa <mahsa.alikhany@gmail.com>
2022-01-07 22:52:55 +0330

    add some properties to OrderDto

--- src/main/java/ir/maktab/model/dto/OrderDto.java
+++ src/main/java/ir/maktab/model/dto/OrderDto.java
@@ -1,4 +1,29 @@
-package ir.maktab.model.dto;/**
+package ir.maktab.model.dto;
+
+import ir.maktab.model.entity.Client;
+import ir.maktab.model.entity.Expert;
+import ir.maktab.model.entity.Offer;
+import ir.maktab.model.entity.SubService;
+import ir.maktab.model.enumaration.OrderStatus;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class OrderDto {
+ */
+public class OrderDto {
+    private long id;
+    private Double proposedPrice;
+    private String description;
+    private Date orderPlacingDate;
+    private Date proposedDateToDo;
+    private String Address;
+    private OrderStatus orderStatus;
+    private Client client;
+    private Expert expert;
+    private SubService subService;
 }
