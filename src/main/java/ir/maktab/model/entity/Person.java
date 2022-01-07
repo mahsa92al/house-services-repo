0c0fa53d8f03c75149a361571431b1c65fd4238f
Mahsa <mahsa.alikhany@gmail.com>
2022-01-07 22:30:11 +0330

    Revert "add @SuperBuilder to Person"
    This reverts commit 8d0724fa1b2195d5877c727cf43a2f6d48861d3c.

--- src/main/java/ir/maktab/model/entity/Person.java
+++ src/main/java/ir/maktab/model/entity/Person.java
@@ -4,7 +4,6 @@ import ir.maktab.model.enumaration.UserStatus;
 import ir.maktab.model.enumaration.Role;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
@@ -15,7 +14,6 @@ import java.util.Date;
  */
 @Setter
 @Getter
-@SuperBuilder
 @MappedSuperclass
 public class Person {
     @Id
