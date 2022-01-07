705a44603329ecf962084e95860095c71367ff88
Mahsa <mahsa.alikhany@gmail.com>
2022-01-07 22:58:56 +0330

    add some properties to ServiceDto

--- src/main/java/ir/maktab/model/dto/ServiceDto.java
+++ src/main/java/ir/maktab/model/dto/ServiceDto.java
@@ -1,4 +1,13 @@
-package ir.maktab.model.dto;/**
+package ir.maktab.model.dto;
+
+import ir.maktab.model.entity.SubService;
+
+import java.util.List;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class ServiceDto {
+ */
+public class ServiceDto {
+    private String title;
+    private List<SubService> subServices;
 }
