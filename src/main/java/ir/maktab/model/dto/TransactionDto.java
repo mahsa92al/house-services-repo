e2d23744f6995a16a2b73255647fc2bcffd04a4a
Mahsa <mahsa.alikhany@gmail.com>
2022-01-07 23:05:43 +0330

    add some properties to TransactionDto

--- src/main/java/ir/maktab/model/dto/TransactionDto.java
+++ src/main/java/ir/maktab/model/dto/TransactionDto.java
@@ -1,4 +1,19 @@
-package ir.maktab.model.dto;/**
+package ir.maktab.model.dto;
+
+import ir.maktab.model.entity.Client;
+import ir.maktab.model.entity.Expert;
+import ir.maktab.model.entity.Order;
+import ir.maktab.model.enumaration.TransactionType;
+
+import java.util.Date;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class TransactionDto {
+ */
+public class TransactionDto {
+    private TransactionType transactionType;
+    private Date transactionDate;
+    private Client client;
+    private Expert expert;
+    private Order order;
 }
