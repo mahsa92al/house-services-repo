f799ab1e1a6319473d9f84b90a46de580de5f748
Mahsa <mahsa.alikhany@gmail.com>
2022-01-07 22:48:22 +0330

    add some properties to CommentDto

--- src/main/java/ir/maktab/model/dto/CommentDto.java
+++ src/main/java/ir/maktab/model/dto/CommentDto.java
@@ -1,4 +1,15 @@
-package ir.maktab.model.dto;/**
+package ir.maktab.model.dto;
+
+import ir.maktab.model.entity.Client;
+import ir.maktab.model.entity.Expert;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class CommentDto {
+ */
+public class CommentDto {
+    private long id;
+    private Double rate;
+    private String comment;
+    private Client client;
+    private Expert expert;
 }
