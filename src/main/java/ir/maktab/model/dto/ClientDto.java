849f42385520225415cb9e0cb3c4d89b5ffbe0c5
Mahsa <mahsa.alikhany@gmail.com>
2022-01-07 22:32:51 +0330

    add some properties to ClientDto file

--- src/main/java/ir/maktab/model/dto/ClientDto.java
+++ src/main/java/ir/maktab/model/dto/ClientDto.java
@@ -1,4 +1,18 @@
-package ir.maktab.model.dto;/**
+package ir.maktab.model.dto;
+
+import ir.maktab.model.enumaration.Role;
+import ir.maktab.model.enumaration.UserStatus;
+
+import java.util.Date;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class ClientDto {
+ */
+public class ClientDto {
+    private String name;
+    private String lastName;
+    private String email;
+    private Date registrationDate;
+    private UserStatus userStatus;
+    private Double credit;
 }
