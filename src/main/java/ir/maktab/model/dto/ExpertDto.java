39b952c6f9106d548901759ed233e4f5564b13b6
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 21:14:18 +0330

    add @Setter and @Getter annotations to ExpertDto

--- src/main/java/ir/maktab/model/dto/ExpertDto.java
+++ src/main/java/ir/maktab/model/dto/ExpertDto.java
@@ -1,12 +1,16 @@
 package ir.maktab.model.dto;
 
 import ir.maktab.model.enumaration.UserStatus;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 /**
  * @author Mahsa Alikhani m-58
  */
+@Setter
+@Getter
 public class ExpertDto {
     private String name;
     private String lastName;
