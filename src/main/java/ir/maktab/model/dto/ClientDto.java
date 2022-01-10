c61cf33bdd79f5907bddfa67b16c50a7c78ca6ae
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 17:36:37 +0330

    add @Builder, @Setter, and @Getter annotations to ClientDto

--- src/main/java/ir/maktab/model/dto/ClientDto.java
+++ src/main/java/ir/maktab/model/dto/ClientDto.java
@@ -1,12 +1,18 @@
 package ir.maktab.model.dto;
 
 import ir.maktab.model.enumaration.UserStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 /**
  * @author Mahsa Alikhani m-58
  */
+@Setter
+@Getter
+@Builder
 public class ClientDto {
     private String name;
     private String lastName;
