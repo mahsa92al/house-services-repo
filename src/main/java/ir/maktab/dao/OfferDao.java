9be21d4c54e1eb1092b508830fab4586c7fbcb5f
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 22:33:01 +0330

    rewrite OfferDao and OfferService method using jpa spring data

--- src/main/java/ir/maktab/dao/OfferDao.java
+++ src/main/java/ir/maktab/dao/OfferDao.java
@@ -1,67 +1,21 @@
 package ir.maktab.dao;
 
+import ir.maktab.model.entity.Expert;
 import ir.maktab.model.entity.Offer;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import ir.maktab.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Mahsa Alikhani m-58
  */
 @Repository
-@RequiredArgsConstructor
-public class OfferDao {
-    private final SessionFactory sessionFactory;
+public interface OfferDao extends JpaRepository<Offer, Long> {
 
-    public void save(Offer offer) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(offer);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public void delete(Offer offer) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query hql = session.createQuery("delete Offer o where o.id=:id");
-        hql.setParameter("id", offer.getId());
-        hql.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public Optional<Offer> findOfferById(Offer offer) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Offer> hql = session.createQuery("from Offer o where o.id=:id");
-        hql.setParameter("id", offer.getId());
-        List<Offer> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(list.isEmpty() ? null : list.get(0));
-    }
-
-    public void update(Offer offer) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(offer);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public List<Offer> findAllOffers() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Offer> hql = session.createQuery("from Offer");
-        List<Offer> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return list;
-    }
+    @Query(value = "select o from Offer o where o.expert = :expert and o.order = :order")
+    List<Offer> findAllOffers(@Param("expert") Expert expert, @Param("order") Order order);
 }
--- src/main/java/ir/maktab/service/OfferService.java
+++ src/main/java/ir/maktab/service/OfferService.java
@@ -2,12 +2,17 @@ package ir.maktab.service;
 
 import ir.maktab.dao.OfferDao;
 import ir.maktab.exception.NotFoundException;
+import ir.maktab.model.dto.OfferDto;
+import ir.maktab.model.entity.Expert;
 import ir.maktab.model.entity.Offer;
+import ir.maktab.model.entity.Order;
+import ir.maktab.service.mapper.OfferMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -16,29 +21,33 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class OfferService {
     private final OfferDao offerDao;
+    private final OfferMapper offerMapper;
 
-    public void addOffer(Offer offer){
+    public void addOffer(OfferDto offerDto){
+        Offer offer = offerMapper.toOffer(offerDto);
         offerDao.save(offer);
     }
 
-    public void removeOffer(Offer offer){
-        Optional<Offer> foundOffer = offerDao.findOfferById(offer);
+    public void removeOffer(OfferDto offerDto){
+        Offer offer = offerMapper.toOffer(offerDto);
+        Optional<Offer> foundOffer = offerDao.findById(offer.getId());
         if(foundOffer.isEmpty())
             throw new NotFoundException("offer not found!");
         offerDao.delete(offer);
     }
 
-    public void update(Offer offer){
-        Optional<Offer> foundOffer = offerDao.findOfferById(offer);
+    public void update(OfferDto offerDto){
+        Offer offer = offerMapper.toOffer(offerDto);
+        Optional<Offer> foundOffer = offerDao.findById(offer.getId());
         if(foundOffer.isEmpty())
             throw new NotFoundException("offer not found!");
-        offerDao.update(offer);
+        offerDao.save(offer);
     }
 
-    public List<Offer> getAllOrders(){
-        List<Offer> offers = offerDao.findAllOffers();
+    public List<OfferDto> getAllOrders(Expert expert, Order order){
+        List<Offer> offers = offerDao.findAllOffers(expert, order);
         if(offers.isEmpty())
             throw new NotFoundException("there is no offer!");
-        return offers;
+        return offers.stream().map(offerMapper::toOfferDto).collect(Collectors.toList());
     }
 }
