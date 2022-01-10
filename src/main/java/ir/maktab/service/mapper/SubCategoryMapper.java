5bffff0f9c4278fc2b704b9b2be2c254d3df9ee6
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 22:57:31 +0330

    add toSubCategory() and toSubCategoryDto() methods to SubCategoryMapper

---
+++ src/main/java/ir/maktab/service/mapper/SubCategoryMapper.java
@@ -0,0 +1,32 @@
+package ir.maktab.service.mapper;
+
+import ir.maktab.model.dto.CategoryDto;
+import ir.maktab.model.dto.SubCategoryDto;
+import ir.maktab.model.entity.Category;
+import ir.maktab.model.entity.SubCategory;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+public class SubCategoryMapper {
+
+    public SubCategory toSubCategory(SubCategoryDto subCategoryDto){
+        return SubCategory.builder()
+                .title(subCategoryDto.getTitle())
+                .basePrice(subCategoryDto.getBasePrice())
+                .description(subCategoryDto.getDescription())
+                .category(subCategoryDto.getCategory())
+                .build();
+    }
+
+    public SubCategoryDto toSubCategoryDto(SubCategory subCategory){
+        return SubCategoryDto.builder()
+                .title(subCategory.getTitle())
+                .basePrice(subCategory.getBasePrice())
+                .description(subCategory.getDescription())
+                .experts(subCategory.getExperts())
+                .category(subCategory.getCategory())
+                .orders(subCategory.getOrders())
+                .build();
+    }
+}
