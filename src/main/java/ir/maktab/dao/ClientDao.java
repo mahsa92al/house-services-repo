eb81a94cb07c87d20c89d5bed0c331c2518657a6
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 18:12:32 +0330

    rewrite Client crud operations in jpa spring data

--- src/main/java/ir/maktab/dao/ClientDao.java
+++ src/main/java/ir/maktab/dao/ClientDao.java
@@ -1,67 +1,17 @@
 package ir.maktab.dao;
 
 import ir.maktab.model.entity.Client;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
  * @author Mahsa Alikhani m-58
  */
 @Repository
-@RequiredArgsConstructor
-public class ClientDao {
-    private final SessionFactory sessionFactory;
+public interface ClientDao extends JpaRepository<Client, Long> {
 
-    public void save(Client client){
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(client);
-        session.getTransaction().commit();
-        session.close();
-    }
+    Optional<Client> findByEmail(String email);
 
-    public Optional<Client> findByEmail(String email) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Client> hql = session.createQuery("from Client c where c.email=:email");
-        hql.setParameter("email", email);
-        List<Client> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(list.isEmpty() ? null : list.get(0));
-    }
-
-    public void update(Client client) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(client);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public List<Client> findAllClients() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Client> hql = session.createQuery("from Client");
-        List<Client> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return list;
-    }
-
-    public void delete(Client client) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query hql = session.createQuery("delete Client c where c.email=:email");
-        hql.setParameter("email", client.getEmail());
-        hql.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-    }
 }
--- src/main/java/ir/maktab/service/ClientService.java
+++ src/main/java/ir/maktab/service/ClientService.java
@@ -3,12 +3,15 @@ package ir.maktab.service;
 import ir.maktab.dao.ClientDao;
 import ir.maktab.exception.DuplicateException;
 import ir.maktab.exception.NotFoundException;
+import ir.maktab.model.dto.ClientDto;
 import ir.maktab.model.entity.Client;
+import ir.maktab.service.mapper.ClientMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -17,6 +20,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ClientService {
     private final ClientDao clientDao;
+    private final ClientMapper clientMapper;
 
     public void add(Client client){
         Optional<Client> found = clientDao.findByEmail(client.getEmail());
@@ -29,14 +33,14 @@ public class ClientService {
         Optional<Client> found = clientDao.findByEmail(client.getEmail());
         if(found.isEmpty())
             throw new NotFoundException("client not found!");
-        clientDao.update(client);
+        clientDao.save(client);
     }
 
-    public List<Client> getAllClients(){
-        List<Client> clients = clientDao.findAllClients();
+    public List<ClientDto> getAllClients(){
+        List<Client> clients = clientDao.findAll();
         if(clients.isEmpty())
             throw new NotFoundException("there is no client!");
-        return clients;
+        return clients.stream().map(clientMapper::toClientDto).collect(Collectors.toList());
     }
 
      public void remove(Client client){
