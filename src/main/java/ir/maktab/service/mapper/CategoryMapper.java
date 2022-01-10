50e1b267b7acd7d86bd83af0cba6972989a148d2
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 16:31:11 +0330

    add toCategory() and toCategoryDto() methods to CategoryMapper

--- src/main/java/ir/maktab/service/mapper/CategoryMapper.java
+++ src/main/java/ir/maktab/service/mapper/CategoryMapper.java
@@ -1,4 +1,24 @@
-package ir.maktab.service.mapper;/**
+package ir.maktab.service.mapper;
+
+import ir.maktab.model.dto.CategoryDto;
+import ir.maktab.model.entity.Category;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class CategoryMapper {
+ */
+public class CategoryMapper {
+
+    public Category toCategory(CategoryDto categoryDto){
+        return Category.builder()
+                .title(categoryDto.getTitle())
+                .subCategories(categoryDto.getSubCategories())
+                .build();
+    }
+
+    public CategoryDto toCategoryDto(Category category){
+        return CategoryDto.builder()
+                .title(category.getTitle())
+                .subCategories(category.getSubCategories())
+                .build();
+    }
 }
