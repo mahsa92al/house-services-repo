e54adc57ded4e070188c87c26223c219b96d0b53
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 22:42:23 +0330

    add @Builder, @Setter, and @Getter annotations to SubCategoryDto

--- src/main/java/ir/maktab/model/dto/SubCategoryDto.java
+++ src/main/java/ir/maktab/model/dto/SubCategoryDto.java
@@ -3,12 +3,18 @@ package ir.maktab.model.dto;
 import ir.maktab.model.entity.Expert;
 import ir.maktab.model.entity.Order;
 import ir.maktab.model.entity.Category;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 /**
  * @author Mahsa Alikhani m-58
  */
+@Setter
+@Getter
+@Builder
 public class SubCategoryDto {
     private String title;
     private Double basePrice;
