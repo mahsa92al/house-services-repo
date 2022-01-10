dd2720cb890500803504e9614a45933b7dc4fb21
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 18:03:26 +0330

    add toClient() and toClientDto() methods to ClientMapper

--- src/main/java/ir/maktab/service/mapper/ClientMapper.java
+++ src/main/java/ir/maktab/service/mapper/ClientMapper.java
@@ -1,4 +1,34 @@
-package ir.maktab.service.mapper;/**
+package ir.maktab.service.mapper;
+
+import ir.maktab.model.dto.CategoryDto;
+import ir.maktab.model.dto.ClientDto;
+import ir.maktab.model.entity.Category;
+import ir.maktab.model.entity.Client;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class ClientMapper {
+ */
+public class ClientMapper {
+    private ClientDto clientDto;
+    private Client client;
+
+    public Client toClient(ClientDto clientDto){
+        client = new Client();
+        client.setName(clientDto.getName());
+        client.setLastName(clientDto.getLastName());
+        client.setEmail(clientDto.getEmail());
+        client.setUserStatus(clientDto.getUserStatus());
+        client.setCredit(clientDto.getCredit());
+        return client;
+    }
+
+    public ClientDto toClientDto(Client client){
+        clientDto = new ClientDto();
+        clientDto.setName(client.getName());
+        clientDto.setLastName(client.getLastName());
+        clientDto.setEmail(client.getEmail());
+        clientDto.setUserStatus(client.getUserStatus());
+        clientDto.setCredit(client.getCredit());
+        return clientDto;
+    }
 }
