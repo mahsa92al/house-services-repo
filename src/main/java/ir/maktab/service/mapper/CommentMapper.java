11b39f5cb5f1d6f7b19bac964db57b253ff5228f
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 20:27:03 +0330

    add toCommit() and toCommitDto() methods to CommentMapper

--- src/main/java/ir/maktab/service/mapper/CommentMapper.java
+++ src/main/java/ir/maktab/service/mapper/CommentMapper.java
@@ -1,4 +1,30 @@
-package ir.maktab.service.mapper;/**
+package ir.maktab.service.mapper;
+
+import ir.maktab.model.dto.CommentDto;
+import ir.maktab.model.entity.Comment;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class CommentMapper {
+ */
+public class CommentMapper {
+
+    public Comment toComment(CommentDto commentDto){
+        return Comment.builder()
+                .id(commentDto.getId())
+                .comment(commentDto.getComment())
+                .rate(commentDto.getRate())
+                .client(commentDto.getClient())
+                .expert(commentDto.getExpert())
+                .build();
+    }
+
+    public CommentDto toCommentDto(Comment comment){
+        return CommentDto.builder()
+                .id(comment.getId())
+                .comment(comment.getComment())
+                .rate(comment.getRate())
+                .client(comment.getClient())
+                .expert(comment.getExpert())
+                .build();
+    }
 }
