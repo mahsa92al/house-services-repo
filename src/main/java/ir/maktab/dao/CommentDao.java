3495593905ad59ca547eeed76165c7c8b20920c6
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 20:47:46 +0330

    rewrite CommentDao and CommentService methods using jpa spring data and comment mapper

--- src/main/java/ir/maktab/dao/CommentDao.java
+++ src/main/java/ir/maktab/dao/CommentDao.java
@@ -1,67 +1,16 @@
 package ir.maktab.dao;
 
 import ir.maktab.model.entity.Comment;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Mahsa Alikhani m-58
  */
 @Repository
-@RequiredArgsConstructor
-public class CommentDao {
-    private final SessionFactory sessionFactory;
+public interface CommentDao extends JpaRepository<Comment, Long> {
 
-    public void save(Comment comment) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(comment);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public void delete(Comment comment) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query hql = session.createQuery("delete Comment c where c.id=:id");
-        hql.setParameter("id", comment.getId());
-        hql.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public Optional<Comment> findCommentById(Comment comment) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Comment> hql = session.createQuery("from Comment c where c.id=:id");
-        hql.setParameter("id", comment.getId());
-        List<Comment> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(list.isEmpty() ? null : list.get(0));
-    }
-
-    public void update(Comment comment) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(comment);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public List<Comment> findAllComments() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Comment> hql = session.createQuery("from Comment ");
-        List<Comment> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return list;
-    }
+    public List<Comment> findAllByExpert_IdOrderByRate(Long expertId);
 }
--- src/main/java/ir/maktab/service/CommentService.java
+++ src/main/java/ir/maktab/service/CommentService.java
@@ -2,12 +2,16 @@ package ir.maktab.service;
 
 import ir.maktab.dao.CommentDao;
 import ir.maktab.exception.NotFoundException;
+import ir.maktab.model.dto.CommentDto;
 import ir.maktab.model.entity.Comment;
+import ir.maktab.model.entity.Expert;
+import ir.maktab.service.mapper.CommentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -16,29 +20,33 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentDao commentDao;
+    private final CommentMapper commentMapper;
 
-    public void addComment(Comment comment){
+    public void addComment(CommentDto commentDto){
+        Comment comment = commentMapper.toComment(commentDto);
         commentDao.save(comment);
     }
 
-    public void removeComment(Comment comment){
-        Optional<Comment> foundComment = commentDao.findCommentById(comment);
+    public void removeComment(CommentDto commentDto){
+        Comment comment = commentMapper.toComment(commentDto);
+        Optional<Comment> foundComment = commentDao.findById(comment.getId());
         if(foundComment.isEmpty())
             throw new NotFoundException("comment not found!");
         commentDao.delete(comment);
     }
 
-    public void update(Comment comment){
-        Optional<Comment> foundComment = commentDao.findCommentById(comment);
+    public void update(CommentDto commentDto){
+        Comment comment = commentMapper.toComment(commentDto);
+        Optional<Comment> foundComment = commentDao.findById(comment.getId());
         if(foundComment.isEmpty())
             throw new NotFoundException("comment not found!");
-        commentDao.update(comment);
+        commentDao.save(comment);
     }
 
-    public List<Comment> getAllComments(){
-        List<Comment> comments = commentDao.findAllComments();
+    public List<CommentDto> getAllComments(Expert expert){
+        List<Comment> comments = commentDao.findAllByExpert_IdOrderByRate(expert.getId());
         if(comments.isEmpty())
             throw new NotFoundException("there is no comment!");
-        return comments;
+        return comments.stream().map(commentMapper::toCommentDto).collect(Collectors.toList());
     }
 }
