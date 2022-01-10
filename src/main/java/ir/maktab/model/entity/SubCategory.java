0be954ed5dcc90d500e62a4c7b3b746fc3f95ee1
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 22:44:54 +0330

    add @Builder, @NoArgsConstructor, and @AllArgsConstructor annotations to SubCategory

--- src/main/java/ir/maktab/model/entity/SubCategory.java
+++ src/main/java/ir/maktab/model/entity/SubCategory.java
@@ -1,7 +1,6 @@
 package ir.maktab.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -12,6 +11,9 @@ import java.util.List;
  */
 @Setter
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class SubCategory {
     @Id
