6658060e814731c44687d65398397706fb829be2
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 20:15:22 +0330

    add @Builder, @NoArgsConstructor, and @AllArgsConstructor to Comment

--- src/main/java/ir/maktab/model/entity/Comment.java
+++ src/main/java/ir/maktab/model/entity/Comment.java
@@ -1,7 +1,6 @@
 package ir.maktab.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -10,6 +9,9 @@ import javax.persistence.*;
  */
 @Setter
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Comment {
     @Id
