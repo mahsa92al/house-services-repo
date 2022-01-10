c48264718e1dee32b2a0df984ec29f1c3362eeb1
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 22:38:39 +0330

    rename OfferService tp OfferServiceImpl and add OfferService interface
    OfferServiceImpl implements OfferService methods

--- src/main/java/ir/maktab/service/OfferService.java
+++ src/main/java/ir/maktab/service/OfferService.java
@@ -1,53 +1,21 @@
 package ir.maktab.service;
 
-import ir.maktab.dao.OfferDao;
-import ir.maktab.exception.NotFoundException;
 import ir.maktab.model.dto.OfferDto;
 import ir.maktab.model.entity.Expert;
-import ir.maktab.model.entity.Offer;
 import ir.maktab.model.entity.Order;
-import ir.maktab.service.mapper.OfferMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * @author Mahsa Alikhani m-58
  */
-@Service
-@RequiredArgsConstructor
-public class OfferService {
-    private final OfferDao offerDao;
-    private final OfferMapper offerMapper;
+public interface OfferService {
 
-    public void addOffer(OfferDto offerDto){
-        Offer offer = offerMapper.toOffer(offerDto);
-        offerDao.save(offer);
-    }
+    void addOffer(OfferDto offerDto);
 
-    public void removeOffer(OfferDto offerDto){
-        Offer offer = offerMapper.toOffer(offerDto);
-        Optional<Offer> foundOffer = offerDao.findById(offer.getId());
-        if(foundOffer.isEmpty())
-            throw new NotFoundException("offer not found!");
-        offerDao.delete(offer);
-    }
+    void removeOffer(OfferDto offerDto);
 
-    public void update(OfferDto offerDto){
-        Offer offer = offerMapper.toOffer(offerDto);
-        Optional<Offer> foundOffer = offerDao.findById(offer.getId());
-        if(foundOffer.isEmpty())
-            throw new NotFoundException("offer not found!");
-        offerDao.save(offer);
-    }
+    void update(OfferDto offerDto);
 
-    public List<OfferDto> getAllOrders(Expert expert, Order order){
-        List<Offer> offers = offerDao.findAllOffers(expert, order);
-        if(offers.isEmpty())
-            throw new NotFoundException("there is no offer!");
-        return offers.stream().map(offerMapper::toOfferDto).collect(Collectors.toList());
-    }
+    List<OfferDto> getAllOrders(Expert expert, Order order);
 }
