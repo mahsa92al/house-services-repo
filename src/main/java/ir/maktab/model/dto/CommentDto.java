23869d014f437814afc018672a7289a55863865b
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 20:12:54 +0330

    add @Setter, @Getter, and @Builder annotations to CommentDto

--- src/main/java/ir/maktab/model/dto/CommentDto.java
+++ src/main/java/ir/maktab/model/dto/CommentDto.java
@@ -2,10 +2,16 @@ package ir.maktab.model.dto;
 
 import ir.maktab.model.entity.Client;
 import ir.maktab.model.entity.Expert;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Mahsa Alikhani m-58
  */
+@Setter
+@Getter
+@Builder
 public class CommentDto {
     private long id;
     private Double rate;
