ec5f6f3135bd3d5638aee486e226167fddf16ac9
Mahsa <mahsa.alikhany@gmail.com>
2022-01-10 16:21:48 +0330

    add @Setter, @Getter, and @Builder annotations to CategoryDto

--- src/main/java/ir/maktab/model/dto/CategoryDto.java
+++ src/main/java/ir/maktab/model/dto/CategoryDto.java
@@ -1,12 +1,18 @@
 package ir.maktab.model.dto;
 
 import ir.maktab.model.entity.SubCategory;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 /**
  * @author Mahsa Alikhani m-58
  */
+@Setter
+@Getter
+@Builder
 public class CategoryDto {
     private String title;
     private List<SubCategory> subCategories;
