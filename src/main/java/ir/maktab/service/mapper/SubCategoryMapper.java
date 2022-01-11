0f28f819e29212caead54842bd458b8618d83259
Mahsa <mahsa.alikhany@gmail.com>
2022-01-11 17:40:33 +0330

    remove non useable imprted packages from SubCategoryMapper

--- src/main/java/ir/maktab/service/mapper/SubCategoryMapper.java
+++ src/main/java/ir/maktab/service/mapper/SubCategoryMapper.java
@@ -1,8 +1,6 @@
 package ir.maktab.service.mapper;
 
-import ir.maktab.model.dto.CategoryDto;
 import ir.maktab.model.dto.SubCategoryDto;
-import ir.maktab.model.entity.Category;
 import ir.maktab.model.entity.SubCategory;
 
 /**
