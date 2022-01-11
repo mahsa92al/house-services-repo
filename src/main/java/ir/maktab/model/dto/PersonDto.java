2cea1c6e419c081bf694b7e8620932e781bce7a1
Mahsa <mahsa.alikhany@gmail.com>
2022-01-11 11:51:51 +0330

    add @Setter, @Getter, and @Builder annotations to PersonDto

--- src/main/java/ir/maktab/model/dto/PersonDto.java
+++ src/main/java/ir/maktab/model/dto/PersonDto.java
@@ -2,12 +2,18 @@ package ir.maktab.model.dto;
 
 import ir.maktab.model.enumaration.UserStatus;
 import ir.maktab.model.enumaration.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 /**
  * @author Mahsa Alikhani m-58
  */
+@Setter
+@Getter
+@Builder
 public class PersonDto {
     private String name;
     private String lastName;
