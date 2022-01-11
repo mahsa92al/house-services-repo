c66eabdb99ec964b802253c1f01b5f87942e83b5
Mahsa <mahsa.alikhany@gmail.com>
2022-01-11 12:01:50 +0330

    add toPerson() and toPersonDto() metods to PersonMapper

--- src/main/java/ir/maktab/service/mapper/PersonMapper.java
+++ src/main/java/ir/maktab/service/mapper/PersonMapper.java
@@ -1,4 +1,34 @@
-package ir.maktab.service.mapper;/**
+package ir.maktab.service.mapper;
+
+import ir.maktab.model.dto.PersonDto;
+import ir.maktab.model.entity.Person;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class PersonMapper {
+ */
+public class PersonMapper {
+
+    private Person person;
+
+    public Person toPerson(PersonDto personDto){
+        person = new Person();
+        person.setName(personDto.getName());
+        person.setLastName(personDto.getLastName());
+        person.setEmail(personDto.getEmail());
+        person.setUserStatus(personDto.getUserStatus());
+        person.setRegistrationDate(personDto.getRegistrationDate());
+        person.setRole(personDto.getRole());
+        return person;
+    }
+
+    public PersonDto toPersonDto(Person person){
+        return PersonDto.builder()
+                .name(person.getName())
+                .lastName(person.getLastName())
+                .email(person.getEmail())
+                .userStatus(person.getUserStatus())
+                .registrationDate(person.getRegistrationDate())
+                .role(person.getRole())
+                .build();
+    }
 }
