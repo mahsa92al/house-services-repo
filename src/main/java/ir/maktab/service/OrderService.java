fa0caa5d9c506479515e105495790ffabf2f15b3
Mahsa <mahsa.alikhany@gmail.com>
2022-01-11 11:49:08 +0330

    rename OrderService to OrderServiceImpl and add OrderService interface
    OrderServiceImpl implements OrderService methods

--- src/main/java/ir/maktab/service/OrderService.java
+++ src/main/java/ir/maktab/service/OrderService.java
@@ -1,48 +1,20 @@
 package ir.maktab.service;
 
-import ir.maktab.dao.OrderDao;
-import ir.maktab.exception.NotFoundException;
 import ir.maktab.model.dto.OrderDto;
 import ir.maktab.model.entity.Order;
-import ir.maktab.service.mapper.OrderMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * @author Mahsa Alikhani m-58
  */
-@Service
-@RequiredArgsConstructor
-public class OrderService {
-    private final OrderDao orderDao;
-    private final OrderMapper orderMapper;
+public interface OrderService {
 
-    public void addOrder(Order order){
-        orderDao.save(order);
-    }
+    void addOrder(Order order);
 
-    public void removeOrder(Order order){
-        Optional<Order> foundOrder = orderDao.findById(order);
-        if(foundOrder.isEmpty())
-            throw new NotFoundException("order not found!");
-        orderDao.delete(order);
-    }
+    void removeOrder(Order order);
 
-    public void update(Order order){
-        Optional<Order> foundOrder = orderDao.findById(order);
-        if(foundOrder.isEmpty())
-            throw new NotFoundException("order not found!");
-        orderDao.save(order);
-    }
+    void update(Order order);
 
-    public List<OrderDto> getAllOrders(){
-        List<Order> orders = orderDao.findAll();
-        if(orders.isEmpty())
-            throw new NotFoundException("there is no order!");
-        return orders.stream().map(orderMapper::toOrderDto).collect(Collectors.toList());
-    }
+    List<OrderDto> getAllOrders();
 }
---
+++ src/main/java/ir/maktab/service/OrderServiceImpl.java
@@ -0,0 +1,52 @@
+package ir.maktab.service;
+
+import ir.maktab.dao.OrderDao;
+import ir.maktab.exception.NotFoundException;
+import ir.maktab.model.dto.OrderDto;
+import ir.maktab.model.entity.Order;
+import ir.maktab.service.mapper.OrderMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService{
+    private final OrderDao orderDao;
+    private final OrderMapper orderMapper;
+
+    @Override
+    public void addOrder(Order order){
+        orderDao.save(order);
+    }
+
+    @Override
+    public void removeOrder(Order order){
+        Optional<Order> foundOrder = orderDao.findById(order);
+        if(foundOrder.isEmpty())
+            throw new NotFoundException("order not found!");
+        orderDao.delete(order);
+    }
+
+    @Override
+    public void update(Order order){
+        Optional<Order> foundOrder = orderDao.findById(order);
+        if(foundOrder.isEmpty())
+            throw new NotFoundException("order not found!");
+        orderDao.save(order);
+    }
+
+    @Override
+    public List<OrderDto> getAllOrders(){
+        List<Order> orders = orderDao.findAll();
+        if(orders.isEmpty())
+            throw new NotFoundException("there is no order!");
+        return orders.stream().map(orderMapper::toOrderDto).collect(Collectors.toList());
+    }
+}
