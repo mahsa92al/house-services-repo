f2f2e62ddfbfcf521045746343a1b6290d68bb40
Mahsa <mahsa.alikhany@gmail.com>
2022-01-11 11:45:14 +0330

    rewrite Order crud operations using jpa spring data

--- src/main/java/ir/maktab/dao/OrderDao.java
+++ src/main/java/ir/maktab/dao/OrderDao.java
@@ -1,67 +1,12 @@
 package ir.maktab.dao;
 
-import ir.maktab.model.entity.Order;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Mahsa Alikhani m-58
  */
 @Repository
-@RequiredArgsConstructor
-public class OrderDao {
-    private final SessionFactory sessionFactory;
+public interface OrderDao extends JpaRepository {
 
-    public void save(Order order) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(order);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public void delete(Order order) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query hql = session.createQuery("delete Order o where o.id=:id");
-        hql.setParameter("id", order.getId());
-        hql.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public Optional<Order> findOrderById(Order order) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Order> hql = session.createQuery("from Order o where o.id=:id");
-        hql.setParameter("id", order.getId());
-        List<Order> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return Optional.ofNullable(list.isEmpty() ? null : list.get(0));
-    }
-
-    public void update(Order order) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(order);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    public List<Order> findAllOrders() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query<Order> hql = session.createQuery("from Order");
-        List<Order> list = hql.getResultList();
-        session.getTransaction().commit();
-        session.close();
-        return list;
-    }
 }
--- src/main/java/ir/maktab/service/OrderService.java
+++ src/main/java/ir/maktab/service/OrderService.java
@@ -2,12 +2,15 @@ package ir.maktab.service;
 
 import ir.maktab.dao.OrderDao;
 import ir.maktab.exception.NotFoundException;
+import ir.maktab.model.dto.OrderDto;
 import ir.maktab.model.entity.Order;
+import ir.maktab.service.mapper.OrderMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -16,29 +19,30 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderDao orderDao;
+    private final OrderMapper orderMapper;
 
     public void addOrder(Order order){
         orderDao.save(order);
     }
 
     public void removeOrder(Order order){
-        Optional<Order> foundOrder = orderDao.findOrderById(order);
+        Optional<Order> foundOrder = orderDao.findById(order);
         if(foundOrder.isEmpty())
             throw new NotFoundException("order not found!");
         orderDao.delete(order);
     }
 
     public void update(Order order){
-        Optional<Order> foundOrder = orderDao.findOrderById(order);
+        Optional<Order> foundOrder = orderDao.findById(order);
         if(foundOrder.isEmpty())
             throw new NotFoundException("order not found!");
-        orderDao.update(order);
+        orderDao.save(order);
     }
 
-    public List<Order> getAllOrders(){
-        List<Order> orders = orderDao.findAllOrders();
+    public List<OrderDto> getAllOrders(){
+        List<Order> orders = orderDao.findAll();
         if(orders.isEmpty())
             throw new NotFoundException("there is no order!");
-        return orders;
+        return orders.stream().map(orderMapper::toOrderDto).collect(Collectors.toList());
     }
 }
