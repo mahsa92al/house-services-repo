333aea7a0cbcbd4172d2c20c36fe6f292af85780
Mahsa <mahsa.alikhany@gmail.com>
2022-01-12 21:37:02 +0330

    add findExpertSubCategoryList method to ExpertDao
    to find expert sub categories

--- src/main/java/ir/maktab/dao/ExpertDao.java
+++ src/main/java/ir/maktab/dao/ExpertDao.java
@@ -1,9 +1,13 @@
 package ir.maktab.dao;
 
 import ir.maktab.model.entity.Expert;
+import ir.maktab.model.entity.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -13,4 +17,7 @@ import java.util.Optional;
 public interface ExpertDao extends JpaRepository<Expert, Long> {
 
     Optional<Expert> findByEmail(String email);
+
+    @Query(value = "select e.subCategories from Expert e where e.email = :email")
+    List<SubCategory> findExpertSubCategoryList(@Param("email") String email);
 }
