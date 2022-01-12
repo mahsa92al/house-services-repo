5232f126a8133e16c4e8c6e426a5c7e227771754
Mahsa <mahsa.alikhany@gmail.com>
2022-01-12 21:43:40 +0330

    modify addOffer() method body and output data type, add tests, tests passed

--- src/main/java/ir/maktab/service/OfferServiceImpl.java
+++ src/main/java/ir/maktab/service/OfferServiceImpl.java
@@ -1,11 +1,15 @@
 package ir.maktab.service;
 
 import ir.maktab.dao.OfferDao;
+import ir.maktab.dao.OrderDao;
 import ir.maktab.exception.NotFoundException;
+import ir.maktab.exception.OfferException;
+import ir.maktab.exception.ProposedPriceException;
 import ir.maktab.model.dto.OfferDto;
 import ir.maktab.model.entity.Expert;
 import ir.maktab.model.entity.Offer;
 import ir.maktab.model.entity.Order;
+import ir.maktab.model.enumaration.OrderStatus;
 import ir.maktab.service.mapper.OfferMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,11 +26,20 @@ import java.util.stream.Collectors;
 public class OfferServiceImpl implements OfferService{
     private final OfferDao offerDao;
     private final OfferMapper offerMapper;
+    private final OrderDao orderDao;
 
     @Override
-    public void addOffer(OfferDto offerDto){
+    public Offer addOffer(OfferDto offerDto){
+        if(!offerDto.getOrder().getOrderStatus().equals(OrderStatus.WAITING_FOR_EXPERT_OFFER))
+            throw new OfferException("the order status is not match!");
+        if(offerDto.getProposedPrice() < offerDto.getOrder().getSubCategory().getBasePrice())
+            throw new ProposedPriceException("the proposed price is less than base price!");
         Offer offer = offerMapper.toOffer(offerDto);
-        offerDao.save(offer);
+        Offer registeredOffer = offerDao.save(offer);
+        Order markedOrder = offer.getOrder();
+        markedOrder.setOrderStatus(OrderStatus.WAITING_TO_CHOOSE_AN_EXPERT);
+        orderDao.save(markedOrder);
+        return registeredOffer;
     }
 
     @Override
