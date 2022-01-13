212e20ff7a453a742385c9f97a325896e05e3753
Mahsa <mahsa.alikhany@gmail.com>
2022-01-13 22:27:26 +0330

    remove CategoryDto and SubCategoryDto, add Category and SubCategory

--- src/test/java/ir/maktab/service/AddSubCategoryTest.java
+++ src/test/java/ir/maktab/service/AddSubCategoryTest.java
@@ -19,32 +19,32 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class AddSubCategoryTest {
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
     SubCategoryServiceImpl subCategoryServiceImpl = context.getBean(SubCategoryServiceImpl.class);
-    CategoryDto categoryDto;
-    SubCategoryDto subCategoryDto;
+    Category category;
+    SubCategory subCategory;
 
     @Test
     void give_Service_And_SubService_when_Add_Calls_Then_NotFoundException_Return(){
-        categoryDto = CategoryDto.builder().title("house facilities").build();
-        subCategoryDto = SubCategoryDto.builder()
+        category = Category.builder().title("house facilities").build();
+        subCategory = SubCategory.builder()
                         .title("water")
                                 .basePrice(100d)
                                         .description("using newest equipment").build();
 
         NotFoundException result = assertThrows(NotFoundException.class, ()->
-                subCategoryServiceImpl.add(categoryDto, subCategoryDto));
+                subCategoryServiceImpl.add(category, subCategory));
         assertEquals("Category not found!", result.getMessage());
     }
 
     @Test
     void give_Service_And_SubService_when_Add_Calls_Then_DuplicateException_Return(){
-        categoryDto = CategoryDto.builder().title("cleaning").build();
-        subCategoryDto = SubCategoryDto.builder()
+        category = Category.builder().title("cleaning").build();
+        subCategory = SubCategory.builder()
                 .title("spraying")
                 .basePrice(50d)
                 .description("Comes with washing").build();
 
         DuplicateException result = assertThrows(DuplicateException.class, ()->
-                subCategoryServiceImpl.add(categoryDto, subCategoryDto));
+                subCategoryServiceImpl.add(category, subCategory));
         assertEquals("Duplicate sub service!", result.getMessage());
     }
 }
